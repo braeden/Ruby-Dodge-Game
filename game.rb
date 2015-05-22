d45891c57af56d2158a86f77154870b701f0315e
Braeden123 <braedensmith2@gmail.com>
2015-05-22 09:27:28 -0400

    commit 20150522

--- game.rb
+++ game.rb
@@ -1,12 +1,11 @@
 require 'ray'
-
+#Define Window Title
 Ray.game "Avoid the Obstacles" do
   register { add_hook :quit, method(:exit!) }
   scene :square do
     @rect = Ray::Polygon.rectangle([0, 0, 20, 20], Ray::Color.blue)
     @rect.pos = [200,200]
-    deleted = 0
-    deletedcon = 0
+    score_num = 0
     level = 0
     oldlevel = 0
     counts = 0
@@ -22,19 +21,23 @@ Ray.game "Avoid the Obstacles" do
       g
     end
     on :mouse_motion do |pos|
+      #Everytime the mouse moves, move rectangle
       @rect.pos = pos
     end
 
     always do
 
       on :key_press, key(:q){ exit }
+      #Quit
       on :key_press, key(:p){
+        #pause
         if level == -3
           level = oldlevel
         else
           oldlevel = level
           level = -3
         end
+        #Not Working yet
       }
       @game_over ||= @obstacles.any? { |e|
         [e.pos.x, e.pos.y, 10, 10].to_rect.collide?([@rect.pos.x, @rect.pos.y, 20,20])
@@ -44,6 +47,7 @@ Ray.game "Avoid the Obstacles" do
     #  }
       @obstacles.each do |e|
         e.pos -= [3 + level, 0 + yrand]
+
       end
 
       @obstacles.reject! { |e|
@@ -60,6 +64,7 @@ Ray.game "Avoid the Obstacles" do
           g
         end
       end
+=begin
       if rand(10) == 5
         @time_slow = 1.times.map do
           x = rand(max_x) + rand(10)
@@ -72,9 +77,10 @@ Ray.game "Avoid the Obstacles" do
 
         end
       end
+=end
 
-      deleted = deleted + 1
-      if deleted % 1000 == 0
+      score_num = score_num + 1
+      if score_num % 1000 == 0
         level += 1
       end
     end
@@ -82,11 +88,11 @@ Ray.game "Avoid the Obstacles" do
       if @game_over
 
         win.draw text("YOU LOST", :at => [180,180], :size => 60)
-        #win.draw text("Score:" + deletedcon.to_s, :at => [220,300], :size => 20)
-      elsif deleted < 200
+        #win.draw text("Score:" + score_numcon.to_s, :at => [220,300], :size => 20)
+      elsif score_num < 200
         win.draw text("Get Ready", :at => [180,180], :size => 60)
       else
-        win.draw text("Score:" + deleted.to_s, :at => [10,10], :size => 20)
+        win.draw text("Score:" + score_num.to_s, :at => [10,10], :size => 20)
         win.draw text("Level:" + level.to_s, :at => [550,10], :size => 20)
         @obstacles.each { |g| win.draw(g) }
         win.draw @rect
